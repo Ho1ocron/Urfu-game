a023878261c7ac4bec995aa4e44a1f750ede5b57
Ho1ocron <cmrfreeze@gmail.com>
2025-10-28 15:53:32 +0500

    main update

--- src/main.py
+++ src/main.py
@@ -23,12 +23,10 @@ class Game(pygame.sprite.Group):
         self._screen = pygame.display.set_mode(self.__screen_size)
 
         self.player_group = pygame.sprite.Group()
-        # try:
         self._player = Knight(10, 10, [], group=self.player_group, speed=5)
+        
         self._screen.fill((0, 0, 0))
         self.player_group.draw(self._screen)
-        # except Exception as main_exception:
-        #     print(f"{main_exception=}")
         
 
     def run(self) -> None:
